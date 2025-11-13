ee679fd7a2f10f2eca410680e925fd0f1ec8e086
fck <fck9961@gmail.com>
2025-11-13 16:53:56 +0800

    vite.config.js change

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import {
   fileURLToPath,
   URL
 } from 'node:url'
-
+// ceshixiugai 
 import {
   defineConfig
 } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
